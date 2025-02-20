e956881ad82a9205bd3163891bd1f93df2e44139
alixanov <alixonovshukurullo13@gmail.com>
2025-02-20 19:40:02 +0500

    138

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(
 // Yo'nalishlar
 app.use("/api", mainRoutes);
 
-const PORT = process.env.PORT || 3008;
+const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
